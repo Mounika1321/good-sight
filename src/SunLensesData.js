7e7a9cb333f5f72c56d7884d8ee49d8dcb598bd6
Mounika <mounikanerlakanti98@gmail.com>
2024-12-09 18:36:21 +1100

    a

--- src/SunLensesData.js
+++ src/SunLensesData.js
@@ -2,15 +2,15 @@ const SunLensesData=[
     {
         ID:1,
         Title:"polaroized lenses",
-        SunLensesImg1:'/SunLenses/Polaroid1.png',
-        SunLensesImg2:'/SunLenses/Polaroid2.png',
+        SunLensesImg1:'SunLenses/Polaroid1.png',
+        SunLensesImg2:'SunLenses/Polaroid2.png',
         Description:"Polarized lenses are specially designed to reduce glare from reflective surfaces such as water, snow, or roads, improving visual clarity and comfort. They work by incorporating a special filter that blocks horizontal light waves, which are responsible for glare. There are various types of polarized lenses, including grey lenses, which reduce overall brightness while maintaining true color perception, and brown lenses, which enhance contrast and depth perception. Additionally, yellow or gold polarized lenses are ideal for low-light conditions, improving visibility during cloudy days. Some lenses are also coated with mirror finishes or come with photochromic properties, adapting to changing light conditions.",                                
     },
     {
         ID:2,
         Title:"Transistion Lenses",
-        SunLensesImg1:'/SunLenses/Tran1.png',
-        SunLensesImg2:'/SunLenses/Tran2.png',
+        SunLensesImg1:'SunLenses/Tran1.png',
+        SunLensesImg2:'SunLenses/Tran2.png',
         Description:"Transitions lenses are advanced photochromic lenses that automatically adjust their tint in response to changing light conditions, becoming darker in bright sunlight and clearer indoors. These lenses provide convenience and comfort by eliminating the need to switch between regular glasses and sunglasses. There are several types of Transitions lenses, including Transitions Signature, which offers fast adaptation and comes in a variety of colors, and Transitions XTRActive, designed for extra protection against bright light and suitable for driving. For those seeking maximum UV protection, Transitions Vantage lenses also include polarization that reduces glare. Additionally, Transitions lenses are available in a range of styles and can be customized to suit individual preferences and needs.",
     },
    
