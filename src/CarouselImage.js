fd52c6e44ad8af148afae89e8dbde4ae9ce7900e
Mounika <mounikanerlakanti98@gmail.com>
2024-12-09 17:45:37 +1100

    a

--- src/CarouselImage.js
+++ src/CarouselImage.js
@@ -8,28 +8,28 @@ import Carousel from 'react-bootstrap/Carousel';
         <div>
               <Carousel>
                 <Carousel.Item>
-                  <img src='/CarouselImages/ray-ban.JPEG'alt="rayban" height="100%" width="100%"/>
+                  <img src='CarouselImages/ray-ban.jpeg'alt="rayban" height="100%" width="100%"/>
                 </Carousel.Item>
                 <Carousel.Item>
-                  <img src='/CarouselImages/Tommy.JPEG' alt="Tommy" height="100%" width="100%"/>      
+                  <img src='/CarouselImages/Tommy.jpeg' alt="Tommy" height="100%" width="100%"/>      
                 </Carousel.Item>
                 <Carousel.Item>
-                  <img src='/CarouselImages/Prada.JPEG' alt="Prada" height="100%" width="100%"/>
+                  <img src='/CarouselImages/Prada.jpeg' alt="Prada" height="100%" width="100%"/>
                 </Carousel.Item>
                 <Carousel.Item>
-                  <img src='/CarouselImages/emporio-armani.JPG' alt="ax" height="100%" width="100%"/>
+                  <img src='/CarouselImages/emporio-armani.jpg' alt="ax" height="100%" width="100%"/>
                 </Carousel.Item>
                 <Carousel.Item>
-                  <img src='/CarouselImages/oakley.JPEG' alt="oakley" height="200%" width="100%"/>      
+                  <img src='/CarouselImages/oakley.jpeg' alt="oakley" height="200%" width="100%"/>      
                 </Carousel.Item>
                 <Carousel.Item>
-                  <img src='/CarouselImages/Swaroski.JPEG' alt="swaroski" height="100%" width="100%"/>
+                  <img src='/CarouselImages/Swaroski.jpeg' alt="swaroski" height="100%" width="100%"/>
                 </Carousel.Item>
                 <Carousel.Item>
-                  <img src='/CarouselImages/Mauvijim.JPEG' alt="mauvi" height="100%" width="100%"/>
+                  <img src='/CarouselImages/Mauvijim.jpeg' alt="mauvi" height="100%" width="100%"/>
                 </Carousel.Item>
                 <Carousel.Item>
-                  <img src='/CarouselImages/Boss.JPG' alt="boss" height="100%" width="100%"/>
+                  <img src='/CarouselImages/Boss.jpg' alt="boss" height="100%" width="100%"/>
                 </Carousel.Item>
                 
               </Carousel>
